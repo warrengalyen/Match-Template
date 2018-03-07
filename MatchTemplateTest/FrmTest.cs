d45feb6a5a0bd91ae8aee23466c674cda48a9e11
warrengalyen <wgalyen@gmail.com>
2018-03-07 12:48:02 -0800

    Add project files.

---
+++ MatchTemplateTest/FrmTest.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using System.Diagnostics;
+
+namespace MatchTemplateTest
+{
+    public unsafe partial class FrmTest : Form
+    {
+        enum IP_DEPTH
+        {
+            IP_DEPTH_8U = 0,
+            IP_DEPTH_8S = 1,
+            IP_DEPTH_16S = 2,
+            IP_DEPTH_32S = 3,
+            IP_DEPTH_32F = 4,
+            IP_DEPTH_64F = 5,
+        };
+
+        enum IP_RET
+        {
+            IP_RET_OK,
+            IP_RET_ERR_OUTOFMEMORY,
+            IP_RET_ERR_STACKOVERFLOW,
+            IP_RET_ERR_NULLREFERENCE,
+            IP_RET_ERR_ARGUMENTOUTOFRANGE,
+            IP_RET_ERR_PARAMISMATCH,
+            IP_RET_ERR_DIVIDEBYZERO,
+            IP_RET_ERR_INDEXOUTOFRANGE,
+            IP_RET_ERR_NOTSUPPORTED,
+            IP_RET_ERR_OVERFLOW,
+            IP_RET_ERR_FILENOTFOUND,
+            IP_RET_ERR_UNKNOWN
+        };
+  
+
+        [StructLayout(LayoutKind.Sequential)]   
+        public unsafe struct TMatrix
+        {
+            public int Width;
+            public int Height;
+            public int WidthStep;
+            public int Channel;
+            public int Depth;
+            public byte* Data;
+            public int Reserved;
+
+            public TMatrix(int Width, int Height, int WidthStep, int Depth, int Channel, byte* Scan0)
+            {
+                this.Width = Width;
+                this.Height = Height;
+                this.WidthStep = WidthStep;
+                this.Depth = Depth;
+                this.Channel = Channel;
+                this.Data = Scan0;
+                this.Reserved = 0;
+            }
+        };
+        
+
+        [DllImport("MatchTemplate.dll", CallingConvention = CallingConvention.StdCall, CharSet = CharSet.Unicode, ExactSpelling = true)]
+        private static extern IP_RET MatchTemplate(ref TMatrix Src, ref TMatrix Template, ref TMatrix *Dest);
+
+        [DllImport("MatchTemplate.dll", CallingConvention = CallingConvention.StdCall, CharSet = CharSet.Unicode, ExactSpelling = true)]
+        private static extern IP_RET MinMaxLoc(ref TMatrix Src, ref int Min_PosX, ref int Min_PosY, ref int Max_PosX, ref int Max_PosY);
+
+        [DllImport("MatchTemplate.dll", CallingConvention = CallingConvention.StdCall, CharSet = CharSet.Unicode, ExactSpelling = true)]
+        private static extern IP_RET IP_FreeMatrix(TMatrix** DstImg);
+
+        Bitmap SrcBmp, DstBmp;
+        TMatrix SrcImg, DestImg;
+
+        public FrmTest()
+        {
+            InitializeComponent();
+        }
+     
+
+        private void CmdOpen_Click(object sender, EventArgs e)
+        {
+
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "All files(*.*) |*.*|Bitmap files (*.Bitmap)|*.Bmp|Jpeg files (*.jpg)|*.jpg|Png files (*.png)|*.png";
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                Bitmap Bmp = (Bitmap)Bitmap.FromFile(openFileDialog.FileName);
+                SrcBmp = (Bitmap)Bmp.Clone();
+                Bmp.Dispose();
+                PicSrc.Image = SrcBmp;
+            }
+        }
+            
+
+
+        private void FrmTest_Load(object sender, EventArgs e)
+        {
+            SrcBmp = (Bitmap)PicSrc.Image;
+            DstBmp = (Bitmap)PicDest.Image;
+     
+        }
+
+        private void CmdSave_Click(object sender, EventArgs e)
+        {
+
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Filter = "Bitmap files (*.Bitmap)|*.Bmp|Jpeg files (*.jpg)|*.jpg|Png files (*.png)|*.png";
+            saveFileDialog.FilterIndex = 3;
+            saveFileDialog.RestoreDirectory = true;
+            if (saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                if (saveFileDialog.FilterIndex == 1)
+                    DstBmp.Save(saveFileDialog.FileName, ImageFormat.Bmp);
+                else if (saveFileDialog.FilterIndex == 2)
+                    DstBmp.Save(saveFileDialog.FileName, ImageFormat.Jpeg);
+                else if (saveFileDialog.FilterIndex == 3)
+                    DstBmp.Save(saveFileDialog.FileName, ImageFormat.Png);
+            }
+        }
+
+        void ShowResult()
+        {
+           
+        }
+
+
+
+        private void CmdProcess_Click(object sender, EventArgs e)
+        {
+             int Min_PosX = 0, Min_PosY = 0, Max_PosX = 0, Max_PosY = 0;
+
+            BitmapData SrcBmpData = SrcBmp.LockBits(new Rectangle(0, 0, SrcBmp.Width, SrcBmp.Height), ImageLockMode.ReadWrite, PixelFormat.Format24bppRgb);
+            BitmapData DstBmpData = DstBmp.LockBits(new Rectangle(0, 0, DstBmp.Width, DstBmp.Height), ImageLockMode.ReadWrite, PixelFormat.Format24bppRgb);
+       
+            Stopwatch Sw = new Stopwatch();
+            Sw.Start();
+
+            SrcImg = new TMatrix(SrcBmp.Width, SrcBmp.Height, SrcBmpData.Stride, (int)IP_DEPTH.IP_DEPTH_8U, 3, (byte*)SrcBmpData.Scan0);
+            DestImg = new TMatrix(DstBmp.Width, DstBmp.Height, DstBmpData.Stride, (int)IP_DEPTH.IP_DEPTH_8U, 3, (byte*)DstBmpData.Scan0);
+
+            TMatrix* Dest = null;
+            MatchTemplate(ref DestImg, ref SrcImg, ref Dest);
+            MinMaxLoc(ref *Dest, ref Min_PosX, ref Min_PosY, ref Max_PosX, ref  Max_PosY);
+            IP_FreeMatrix(&Dest);
+            SrcBmp.UnlockBits(SrcBmpData);
+            DstBmp.UnlockBits(DstBmpData);
+
+            Pen P = new Pen(Color.Red);
+            Graphics G = Graphics.FromImage(DstBmp);
+            G.DrawRectangle(P, new Rectangle(Min_PosX, Min_PosY, SrcImg.Width, SrcImg.Height));
+            P.Dispose();
+            G.Dispose();
+        
+            LblInfo.Text = "Image size: " + PicSrc.Image.Width.ToString() + " X " + PicSrc.Image.Height.ToString() + "\nAlgorithm processing time "  + Sw.ElapsedMilliseconds.ToString() + " ms";
+            PicDest.Refresh();
+        }
+
+      
+       
+
+    }
+}
---
+++ MatchTemplateTest/FrmTest.designer.cs
@@ -0,0 +1,136 @@
+﻿namespace MatchTemplateTest
+{
+    unsafe partial class FrmTest
+    {
+        /// <summary>
+        /// Required designer variables.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up all the resources in use.
+        /// </summary>
+        /// <param name="disposing">True if the managed resource should be released; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Method Required for Designer Support
+        /// Do not modify the contents of this method using a code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmTest));
+            this.CmdOpen = new System.Windows.Forms.Button();
+            this.CmdSave = new System.Windows.Forms.Button();
+            this.PicDest = new System.Windows.Forms.PictureBox();
+            this.PicSrc = new System.Windows.Forms.PictureBox();
+            this.LblInfo = new System.Windows.Forms.Label();
+            this.CmdProcess = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.PicDest)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PicSrc)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // CmdOpen
+            // 
+            this.CmdOpen.Location = new System.Drawing.Point(4, 7);
+            this.CmdOpen.Name = "CmdOpen";
+            this.CmdOpen.Size = new System.Drawing.Size(75, 31);
+            this.CmdOpen.TabIndex = 21;
+            this.CmdOpen.Text = "Open image";
+            this.CmdOpen.UseVisualStyleBackColor = true;
+            this.CmdOpen.Click += new System.EventHandler(this.CmdOpen_Click);
+            // 
+            // CmdSave
+            // 
+            this.CmdSave.Location = new System.Drawing.Point(85, 7);
+            this.CmdSave.Name = "CmdSave";
+            this.CmdSave.Size = new System.Drawing.Size(75, 31);
+            this.CmdSave.TabIndex = 35;
+            this.CmdSave.Text = "Save image";
+            this.CmdSave.UseVisualStyleBackColor = true;
+            this.CmdSave.Click += new System.EventHandler(this.CmdSave_Click);
+            // 
+            // PicDest
+            // 
+            this.PicDest.Image = ((System.Drawing.Image)(resources.GetObject("PicDest.Image")));
+            this.PicDest.Location = new System.Drawing.Point(12, 55);
+            this.PicDest.Name = "PicDest";
+            this.PicDest.Size = new System.Drawing.Size(960, 540);
+            this.PicDest.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.PicDest.TabIndex = 34;
+            this.PicDest.TabStop = false;
+            // 
+            // PicSrc
+            // 
+            this.PicSrc.Image = ((System.Drawing.Image)(resources.GetObject("PicSrc.Image")));
+            this.PicSrc.Location = new System.Drawing.Point(1005, 55);
+            this.PicSrc.Name = "PicSrc";
+            this.PicSrc.Size = new System.Drawing.Size(39, 36);
+            this.PicSrc.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.PicSrc.TabIndex = 33;
+            this.PicSrc.TabStop = false;
+            // 
+            // LblInfo
+            // 
+            this.LblInfo.AutoSize = true;
+            this.LblInfo.Location = new System.Drawing.Point(269, 15);
+            this.LblInfo.Name = "LblInfo";
+            this.LblInfo.Size = new System.Drawing.Size(25, 13);
+            this.LblInfo.TabIndex = 36;
+            this.LblInfo.Text = "      ";
+            // 
+            // CmdProcess
+            // 
+            this.CmdProcess.Location = new System.Drawing.Point(166, 7);
+            this.CmdProcess.Name = "CmdProcess";
+            this.CmdProcess.Size = new System.Drawing.Size(75, 31);
+            this.CmdProcess.TabIndex = 37;
+            this.CmdProcess.Text = "Process";
+            this.CmdProcess.UseVisualStyleBackColor = true;
+            this.CmdProcess.Click += new System.EventHandler(this.CmdProcess_Click);
+            // 
+            // FrmTest
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1257, 706);
+            this.Controls.Add(this.CmdProcess);
+            this.Controls.Add(this.LblInfo);
+            this.Controls.Add(this.CmdSave);
+            this.Controls.Add(this.PicDest);
+            this.Controls.Add(this.PicSrc);
+            this.Controls.Add(this.CmdOpen);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MinimizeBox = false;
+            this.Name = "FrmTest";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Match Template Test";
+            this.Load += new System.EventHandler(this.FrmTest_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.PicDest)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PicSrc)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button CmdOpen;
+        private System.Windows.Forms.PictureBox PicSrc;
+        private System.Windows.Forms.PictureBox PicDest;
+        private System.Windows.Forms.Button CmdSave;
+        private System.Windows.Forms.Label LblInfo;
+        private System.Windows.Forms.Button CmdProcess;
+
+    }
+}
+
